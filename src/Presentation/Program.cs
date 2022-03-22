d8eb4eed7145c6e7b22f1f8fc739fcf2d3192bf5
TBiannca <bia.topircean@gmail.com>
2022-03-23 00:23:49 +0200

    [feature/9] Connect to database

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,4 +1,11 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Context>(options => options.UseMySQL(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
