7ef8ebd6c35600aa5d744daf8a355bfa0221576c
TBiannca <bia.topircean@gmail.com>
2022-03-23 22:41:43 +0200

    [feature/10] Ensure database is created

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -3,11 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetSection("ConnectionString").Value;
 builder.Services.AddDbContext<Context>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    using var serviceScope = ((IApplicationBuilder) app).ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope();
+    var context = serviceScope.ServiceProvider.GetRequiredService<Context>();
+    context.Database.EnsureCreated();
+
+    return "Hello World!";
+});
 
 app.Run();
