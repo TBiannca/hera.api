08a88a616979566558fbcd197955d1d92bb21ac5
TBiannca <bia.topircean@gmail.com>
2022-03-27 01:02:03 +0200

    [feature/18] Create admin user

--- src/Presentation/Auth/Controller.cs
+++ src/Presentation/Auth/Controller.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Auth;
@@ -5,10 +6,18 @@ namespace Presentation.Auth;
 [Route("auth")]
 public class Controller : Microsoft.AspNetCore.Mvc.Controller
 {
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public Controller(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
     [Route("register")]
     [HttpPost]
-    public IActionResult Register()
+    public async Task<IActionResult> Register()
     {
+        IdentityUser user = new IdentityUser(){ UserName = "admin" };
+        await _userManager.CreateAsync(user, "Admin123!");
         return Ok();
     }
 }
