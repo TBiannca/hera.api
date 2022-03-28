79e66b234285d18c0f88b1faeff5f706ded954a4
TBiannca <bia.topircean@gmail.com>
2022-03-28 23:57:24 +0300

    [feature/22] Create /login route

--- src/Presentation/Auth/Controller.cs
+++ src/Presentation/Auth/Controller.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Presentation.Auth;
 
@@ -12,6 +16,7 @@ public class Controller : Microsoft.AspNetCore.Mvc.Controller
     {
         _userManager = userManager;
     }
+    
     [Route("register")]
     [HttpPost]
     public async Task<IActionResult> Register([FromBody]Credentials credentials)
@@ -21,5 +26,47 @@ public class Controller : Microsoft.AspNetCore.Mvc.Controller
         var statusCode = result.Succeeded ? 200 : 400;
 
         return StatusCode(statusCode, result);
+    }
+
+    [Route("login")]
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] Credentials credentials)
+    {
+        var user = await _userManager.FindByNameAsync(credentials.UserName);
+        
+        if (user != null && await _userManager.CheckPasswordAsync(user, credentials.Password))
+        {
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var token = GetToken(authClaims);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+        return Unauthorized();
+    }
+    
+    private JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        var bytes = Encoding.UTF8.GetBytes("this is my custom Secret key for authentication");
+        var authSigningKey = new SymmetricSecurityKey(bytes);
+
+        var token = new JwtSecurityToken(
+            issuer: "https://localhost:7216/",
+            audience: "https://localhost:7216/",
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return token;
     }
 }
