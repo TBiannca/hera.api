217eb86f8dbe800c8af6341fbeaf8052c6d233f5
TBiannca <bia.topircean@gmail.com>
2022-05-03 22:31:27 +0300

    [feature/27] Add role and descriptors to Persons Table

--- src/Data/EPerson.cs
+++ src/Data/EPerson.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Nodes;
+
 namespace Data;
 
 public class EPerson
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Role { get; set; }
+    public string Descriptors { get; set; }
 }
---
+++ src/Data/Migrations/20220503192907_Add_Role_And_Descriptors.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class Add_Role_And_Descriptors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descriptors",
+                table: "Persons",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Persons",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descriptors",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Persons");
+        }
+    }
+}
